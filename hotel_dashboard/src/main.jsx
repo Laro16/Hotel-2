8a63850fe5a2e27cbefef847605b2f22beae0e7d
Laro16 <luis21aro@gmail.com>
2025-10-14 17:28:53 -0600

    Delete hotel_dashboard directory

--- hotel_dashboard/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HotelDashboard from './HotelDashboard'
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<HotelDashboard />)
